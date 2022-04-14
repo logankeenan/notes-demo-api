ec41cfd5399f6ff5c02121eccd4b81e5c5d4a110
Logan Keenan <d.logankeenan@gmail.com>
2022-04-13 20:38:50 -0500

    clean up

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,5 @@ module NotesApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
-    # config.middleware.use ActionDispatch::Session::CookieStore
-    # config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
   end
 end
